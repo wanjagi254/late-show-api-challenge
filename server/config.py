f5b9ef2fc6c8248c3a5450d218e61a60c5d25707
wanjagi254 <githinjibrian700@gmail.com>
2025-06-27 09:28:30 -0700

    added changes

--- server/config.py
+++ server/config.py
@@ -1,9 +1,6 @@
-import os
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "postgresql://user:wanjagi87@localhost:5432/late_show_db"
-    )
+    SQLALCHEMY_DATABASE_URI = "postgresql://user:wanjagi87@localhost:5432/late_show_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-jwt-key")  # Change this!
+    SECRET_KEY = "your_secret_key"
+    JWT_SECRET_KEY = "your_jwt_secret_key"
+    
